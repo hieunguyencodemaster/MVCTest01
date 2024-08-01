6beea15255d9a4d897bc41f72237992d82ed12fb
NguyenTrongHieu <nguyentronghieu14032003@gmail.com>
2024-08-02 04:43:57 +0700

    Test02

--- MVCTest01/obj/Debug/net8.0/MVCTest01.AssemblyInfo.cs
+++ MVCTest01/obj/Debug/net8.0/MVCTest01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCTest01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af6375434b9c21c928fc0828dc603855ee6ac0d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCTest01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCTest01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
